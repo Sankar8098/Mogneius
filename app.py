c351e9731ef97788f79fc0d4461a63384c78f676
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-01-10 14:44:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
